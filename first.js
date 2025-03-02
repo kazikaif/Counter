fb306302cb7f26e2e5d4698e10e1c0d14bde97a8
Kazi Mohammad Kaif <kaifkazi83@gmail.com>
2025-03-02 13:02:24 +0530

    Added

--- first.js
+++ first.js
@@ -2,6 +2,7 @@ let count = document.getElementById("count");
 let btnDecrease = document.querySelector("#btn");
 let btnincrease = document.querySelector("#btn1");
 let clear = document.querySelector("#btn2");
+let err = document.querySelector(".error");
  
 
  let countnumber = 0;
@@ -9,15 +10,26 @@ let clear = document.querySelector("#btn2");
  
 btnDecrease.addEventListener("click",()=>{
     
-    countnumber--;
-    count.textContent = countnumber;
+    if(countnumber > 0){
+        countnumber--;
+        count.textContent = countnumber;
+        err.textContent="";
+    }
+    else{
+        err.textContent = "Can't Decrease below 0";
+        err.style.color = "red";
+    }
 });
 
 btnincrease.addEventListener("click",()=>{
     countnumber++;
     count.textContent = countnumber;
+    err.textContent="";
+
 });
 clear.addEventListener(("click"),()=>{
      countnumber =0;
      count.textContent = countnumber;
+     err.textContent="";
+
  })
